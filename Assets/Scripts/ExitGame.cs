0dd00d35c406798c66e76de119a0e3ba7694c598
Dmitry M <linksum@mail.ru>
2019-05-08 19:31:48 +0300

    Add main menu

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+        // убивает процесс Юнити
+        //System.Diagnostics.Process.GetCurrentProcess().Kill();
+
+    }
+}
---
+++ Assets/Scripts/VolumeLevelHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeLevelHelper : MonoBehaviour
+{
+    private AudioSource source;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        source.volume = Settings.soundlevel;
+    }
+}
