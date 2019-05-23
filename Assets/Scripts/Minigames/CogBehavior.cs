b480024a6aadbd3b70d079e98ddd882c3020f489
VladimirMaltsev <vov.maltsev@gmail.com>
2019-05-23 18:14:09 +0300

    include cog game

---
+++ Assets/Scripts/Minigames/CogBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CogBehavior : MonoBehaviour
+{
+    public bool isRotate = false;
+    public int dir = 1;
+    public bool isStatic = false;
+    public List<GameObject> childs = new List<GameObject>();
+    
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.tag == "cog")
+        {
+            
+            if (coll.gameObject.GetComponent<CogBehavior>().isRotate && !this.isRotate)
+            {
+                if (!isStatic)
+                    coll.gameObject.GetComponent<CogBehavior>().childs.Add(this.gameObject);
+                isRotate = true;
+                dir = coll.gameObject.GetComponent<CogBehavior>().dir * (-1);
+            };
+        }
+    }
+    void Update()
+    {
+        if (isRotate)
+            transform.Rotate(0, 1*dir, 0, Space.Self);
+    }
+    
+    void OnMouseDrag()
+    {
+        if (!isStatic && isRotate)
+        {
+            StopRotation();
+            
+        }
+    }
+    public void StopRotation()
+    {
+        if (!isRotate)
+            return;
+        isRotate = false;
+        foreach (GameObject o in childs)
+        {
+            o.GetComponent<CogBehavior>().StopRotation();
+        }
+        childs.Clear();
+    }
+}
---
+++ Assets/Scripts/Minigames/Pivots.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pivots : MonoBehaviour
+{
+    public bool[] isUsed;
+    public GameObject[] pivots;
+
+    public int Size()
+    {
+        return pivots.Length;
+    }
+
+    public GameObject Get(int i)
+    {
+        return pivots[i];
+    }
+
+    public void Occupy(int i)
+    {
+        isUsed[i] = true;
+    }
+
+    public void Free(int i)
+    {
+        isUsed[i] = false;
+    }
+
+    public bool IsUsed(int i)
+    {
+        return isUsed[i];
+    }
+}
